151a99ea850540cd85520efa33000ad8606467f8
Tauseef Khan <tauseef25@gmail.com>
2025-12-29 18:34:50 -0800

    draw 1 point

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,6 +1,38 @@
 const COLOR = '#519872';
 const CLEAR_COLOR = '#585464';
 const CANVAS_SIZE = 800;
+const POINT_SIZE = 10;
+
+type Dimensions = {
+  width: number;
+  height: number;
+};
+
+const SCREEN_DIMENSIONS: Dimensions = {
+  width: CANVAS_SIZE,
+  height: CANVAS_SIZE,
+};
+
+interface Point2D {
+  x: number;
+  y: number;
+}
+
+/**
+ * Convert NDC coordinates to screen space coordinates
+ *
+ * [-1, -1] -> [1, 1]
+ * [ 0,  0] -> [w, h]
+ */
+export const screen = (
+  { x, y }: Point2D,
+  screenDimensions: Dimensions,
+): Point2D => {
+  return {
+    x: ((x + 1) / 2) * screenDimensions.width,
+    y: (1 - (y + 1) / 2) * screenDimensions.height,
+  };
+};
 
 export function setupCanvas2d(element: HTMLCanvasElement) {
   const init = () => {
@@ -16,7 +48,8 @@ export function setupCanvas2d(element: HTMLCanvasElement) {
     context2d.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
     context2d.fillStyle = COLOR;
-    context2d.rect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    const point1 = screen({ x: -0.5, y: 0.5 }, SCREEN_DIMENSIONS);
+    context2d.rect(point1.x, point1.y, POINT_SIZE, POINT_SIZE);
     context2d.fill();
   };
 
