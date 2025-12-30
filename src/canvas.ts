ee0fbedafb5f99dbd45bf6d9427db70b477f8c39
Tauseef Khan <tauseef25@gmail.com>
2025-12-29 21:49:57 -0800

    rotate and translate

--- src/canvas.ts
+++ src/canvas.ts
@@ -46,15 +46,15 @@ export const screen = (
 
 const POINTS: Point3D[] = [
   // front face
-  { x: -0.5, y: 0.5, z: 1.0 },
-  { x: 0.5, y: 0.5, z: 1.0 },
-  { x: 0.5, y: -0.5, z: 1.0 },
-  { x: -0.5, y: -0.5, z: 1.0 },
+  { x: -0.5, y: 0.5, z: -0.5 },
+  { x: 0.5, y: 0.5, z: -0.5 },
+  { x: 0.5, y: -0.5, z: -0.5 },
+  { x: -0.5, y: -0.5, z: -0.5 },
   // back face
-  { x: -0.5, y: 0.5, z: 2.0 },
-  { x: 0.5, y: 0.5, z: 2.0 },
-  { x: 0.5, y: -0.5, z: 2.0 },
-  { x: -0.5, y: -0.5, z: 2.0 },
+  { x: -0.5, y: 0.5, z: 0.5 },
+  { x: 0.5, y: 0.5, z: 0.5 },
+  { x: 0.5, y: -0.5, z: 0.5 },
+  { x: -0.5, y: -0.5, z: 0.5 },
 ];
 
 const FACES = [
@@ -69,50 +69,69 @@ const FACES = [
 const FPS = 60;
 const MILLIS_PER_FRAME = 1000;
 
+const rotate_xz = ({ x, y, z }: Point3D, angle: number) => {
+  const c = Math.cos(angle);
+  const s = Math.sin(angle);
+  return {
+    x: x * c - z * s,
+    y,
+    z: x * s + z * c,
+  };
+};
+
+const translate = ({ x, y, z }: Point3D, dz: number) => {
+  return { x, y, z: z + dz };
+};
+
 export function setupCanvas2d(element: HTMLCanvasElement) {
-  let context2d: CanvasRenderingContext2D | null = null;
+  element.width = CANVAS_SIZE;
+  element.height = CANVAS_SIZE;
 
-  const init = () => {
-    element.width = CANVAS_SIZE;
-    element.height = CANVAS_SIZE;
+  const context2d = element.getContext('2d');
 
-    context2d = element.getContext('2d');
-    if (!context2d) {
-      throw new Error('Failed to get drawing context');
-    }
+  if (!context2d) {
+    throw new Error('Failed to get drawing context');
+  }
 
+  let angle = 0;
+  const dt = 1 / FPS;
+
+  const clear = () => {
     context2d.fillStyle = CLEAR_COLOR;
     context2d.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-
-    context2d.strokeStyle = COLOR;
-    context2d.lineWidth = 2;
   };
 
   const draw = () => {
-    if (!context2d) {
-      throw new Error('Failed to get drawing context');
-    }
+    clear();
 
+    context2d.strokeStyle = COLOR;
+    context2d.lineWidth = 2;
+
+    angle += (Math.PI * dt) / 2;
     context2d.beginPath();
 
     for (const face of FACES) {
       for (let idx = 0; idx < face.length; idx++) {
-        const from = screen(project(POINTS[face[idx]]), SCREEN_DIMENSIONS);
-        const to = screen(
-          project(POINTS[face[(idx + 1) % face.length]]),
+        const from = POINTS[face[idx]];
+        const to = POINTS[face[(idx + 1) % face.length]];
+
+        const projectedFrom = screen(
+          project(translate(rotate_xz(from, angle), 2.0)),
+          SCREEN_DIMENSIONS,
+        );
+        const projectedTo = screen(
+          project(translate(rotate_xz(to, angle), 2.0)),
           SCREEN_DIMENSIONS,
         );
 
-        context2d.moveTo(from.x, from.y);
-        context2d.lineTo(to.x, to.y);
+        context2d.moveTo(projectedFrom.x, projectedFrom.y);
+        context2d.lineTo(projectedTo.x, projectedTo.y);
       }
     }
     context2d.stroke();
 
     setTimeout(draw, FPS / MILLIS_PER_FRAME);
   };
-
-  init();
 
   draw();
 }
